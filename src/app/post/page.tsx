ef337fb0837c536980ddc89dc92ab70dbe58b291
Farid Vatani <faridvatani@gmail.com>
2023-07-29 12:07:49 +0330

    async server component & loading

---
+++ src/app/post/page.tsx
@@ -0,0 +1,23 @@
+// asynchronous server component  by using https://jsonplaceholder.typicode.com/posts/1
+import { FC } from "react";
+
+interface pageProps {}
+
+const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const page: FC<pageProps> = async ({}) => {
+  await wait(5000);
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "GET",
+  });
+  const data = await response.json();
+
+  return (
+    <>
+      <div>Dashboard Page</div>
+      <h2>{data.body}</h2>
+    </>
+  );
+};
+
+export default page;
